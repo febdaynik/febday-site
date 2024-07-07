670c2c0fdcbc84336433d7865eaa87dbc5df417c
febdaynik <drons_dron@mail.ru>
2024-07-07 04:02:37 +0300

    Небольшое исправление по адаптиву и ограничения для мобильных устройств

--- src/components/Desktop/desktopPage.js
+++ src/components/Desktop/desktopPage.js
@@ -1,3 +1,5 @@
+import { useMediaQuery } from 'react-responsive';
+
 import WatchComponent from './WatchBlock/watch';
 import SettingComponent from './SettingBlock/setting';
 import LanguageComponent from './LanguageBlock/language';
@@ -7,26 +9,43 @@ import DesktopPanelComponent from './DesktopPanel/desktopPanel';
 export default function DesktopPage() {
     import('../../styles/desktopPage.css')
 
-    return (
-        <div className='desktop-block'>
-            <header className="desktop-header">
-                <div className="hover-header multi-page-hover">
-                    <div className="multi-page-button selected" />
-                    <div className="multi-page-button" />
-                </div>
-                <WatchComponent />
-                <nav className="system-block">
-                    <LanguageComponent />
-                    <SettingComponent />
-                </nav>
-            </header>
+    const isDesktopOrLaptop = useMediaQuery(
+		{ minDeviceWidth: 1024 },
+	)
 
-            <LeftPanelComponent />
-            
-            <div className="desktop">
-                <DesktopPanelComponent />
+    return (
+        isDesktopOrLaptop ? (
+            <div className='desktop-block'>
+                <header className="desktop-header">
+                    <div className="hover-header multi-page-hover">
+                        <div className="multi-page-button selected" />
+                        <div className="multi-page-button" />
+                    </div>
+                    <WatchComponent />
+                    <nav className="system-block">
+                        <LanguageComponent />
+                        <SettingComponent />
+                    </nav>
+                </header>
+
+                <LeftPanelComponent />
+                
+                <div className="desktop">
+                    <DesktopPanelComponent />
+                </div>
             </div>
-        </div>
+        ) : (
+            <div>
+                <h1 style={{
+                    color: "var(--white)",
+                    position: "absolute", 
+                    top: "45%", left: "50%", 
+                    transform: "translate(-50%, -50%)",
+                    fontSize: "6.5dvb",
+                    textAlign: "center",
+                }}>Данная страница пока что не доступна с мобильного устройства</h1>
+            </div>
+        )
     )
 
 }
--- src/components/Menu/menu.js
+++ src/components/Menu/menu.js
@@ -1,10 +1,11 @@
 import { motion } from 'framer-motion';
+import { useMediaQuery } from 'react-responsive';
 
 import "../../styles/Menu.css";
 import "../../styles/TransitionPage.css";
-import githubLogo from "../../images/github-mark-white.png"
-import CustomLink from "../AnimateLink"
-import ButtonAnimate from "./buttonAnimate"
+import githubLogo from "../../images/github-mark-white.png";
+import CustomLink from "./AnimateLink";
+import ButtonAnimate from "./buttonAnimate";
 
 
 const animateTemplate = async (
@@ -17,7 +18,7 @@ const animateTemplate = async (
 	backgroundMenu.classList.add(className);
 	await new Promise(resolve => setTimeout(resolve, duration));
 	backgroundMenu.classList.remove(className);
-}
+};
 
 const animateIn = async () => {
 	if ( localStorage.getItem('animate') !== null && !Number(localStorage.getItem('animate')))
@@ -28,7 +29,7 @@ const animateIn = async () => {
 	menuBlock.style.opacity = 0;
 
 	return await animateTemplate('background-menu-block-animation', 2800)
-}
+};
 
 const animateOut = async () => {
 	if ( localStorage.getItem('animate') !== null && !Number(localStorage.getItem('animate')) )
@@ -39,15 +40,22 @@ const animateOut = async () => {
 	menuBlock.style.opacity = 1;
 
 	return await animateTemplate('background-menu-block-out-animation', 3000)
-}
+};
 
 const githubClick = (value) => {
 	window.open("https://github.com/febdaynik/febday-site");
 };
 
-export default function Menu() {
+const mobileMenu = () => {
+	document.querySelector('.menu-block').classList.toggle("visible");
+	document.querySelector('.open-menu').classList.toggle("visible");
+};
 
-	
+export default function Menu() {
+	const isDesktopOrLaptop = useMediaQuery(
+		{ minDeviceWidth: 1024 },
+	)
+
 	return (
 		<motion.div 
 			animate="active"
@@ -55,29 +63,56 @@ export default function Menu() {
 			onAnimationStart={animateOut}
 			style={{height: "100%", width: "100"}}
 		>	
-			<CustomLink to="/desktop" onClick={animateIn} style={{width: "100%", height: "100%", position: "absolute", perspective: "64px", cursor: "auto"}}>
-				<div className="computer-screen"></div>
-			</CustomLink>
-			<div className="menu-block">
-				<div>
-					<div className="div-h1-block">
-						<h1>МЕНЮ</h1>
+			{ isDesktopOrLaptop ? (
+				<>
+					<CustomLink to="/desktop" onClick={animateIn} style={{ width: "100%", height: "100%", position: "absolute", perspective: "64px", cursor: "auto" }}>
+						<div className="computer-screen"></div>
+					</CustomLink>
+					<div className="menu-block">
+						<div>
+							<div className="div-h1-block">
+								<h1>МЕНЮ</h1>
+							</div>
+							<nav className="menu-block-nav">
+								<div><span><CustomLink to="/desktop/botofolio" onClick={animateIn}>Ботофолио</CustomLink></span></div>
+								<div><span><CustomLink to="/desktop/about" onClick={animateIn}>Обо мне</CustomLink></span></div>
+								<div><span><CustomLink to="/desktop/contact" onClick={animateIn}>Контакты</CustomLink></span></div>
+							</nav>
+							<div className="menu-footer">
+								<div id="githubbbutton" onClick={githubClick}>
+									<img src={githubLogo} alt="github logo" />
+									<span>source code</span>
+								</div>
+							</div>
+						</div>
+						<ButtonAnimate />
 					</div>
-					<nav className="menu-block-nav">
-						<div><span><CustomLink to="/desktop/botofolio" onClick={ animateIn }>Ботофолио</CustomLink></span></div>
-						<div><span><CustomLink to="/desktop/about" onClick={ animateIn }>Обо мне</CustomLink></span></div>
-						<div><span><CustomLink to="/desktop/contact" onClick={ animateIn }>Контакты</CustomLink></span></div>
-					</nav>
-					<div className="menu-footer">
-						<div id="githubbbutton" onClick={githubClick}>
-							<img src={githubLogo} alt="github logo" />
-							<span>source code</span>
+				</>
+			) : 
+				<>
+					<button className="open-menu" onClick={mobileMenu}>
+						<div className="burger-button"></div>
+					</button>
+					<div className="menu-block">
+						<div>
+							<div className="div-h1-block">
+								<h1>МЕНЮ</h1>
+							</div>
+							<nav className="menu-block-nav">
+								<div><span><CustomLink to="/desktop/botofolio" onClick={animateIn}>Ботофолио</CustomLink></span></div>
+								<div><span><CustomLink to="/desktop/about" onClick={animateIn}>Обо мне</CustomLink></span></div>
+								<div><span><CustomLink to="/desktop/contact" onClick={animateIn}>Контакты</CustomLink></span></div>
+							</nav>
+							<div className="menu-footer">
+								<div id="githubbbutton" onClick={githubClick}>
+									<img src={githubLogo} alt="github logo" />
+									<span>source code</span>
+								</div>
+							</div>
 						</div>
 					</div>
-				</div>
-				<ButtonAnimate />
-
-			</div>
+				</>
+			}
 			<div className="background-menu-block"/>
 		</motion.div>
 	);
