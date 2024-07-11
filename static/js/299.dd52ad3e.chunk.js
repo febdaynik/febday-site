c2df29dd79136fe2a6f358ddd99d0a2470c99347
febdaynik <drons_dron@mail.ru>
2024-07-11 04:37:11 +0300

    Updates

---
+++ static/js/299.dd52ad3e.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmy_app=self.webpackChunkmy_app||[]).push([[299],{299:(p,e,s)=>{s.r(e),s.d(e,{default:()=>a});const a={}}}]);
+//# sourceMappingURL=299.dd52ad3e.chunk.js.map
